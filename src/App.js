2b785a267b2e9b02cf4e7ab1cca5082d926a542c
Abdulhakim <abdlhakimnesrugmail.com>
2025-09-22 19:23:30 +0300

    category part

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import CarouselEffect from './Components/Carousel/Carousel'; // match file name exactly
+import Category from './Components/Category/Category';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <CarouselEffect />
+      <Category />
     </div>
   );
 }
